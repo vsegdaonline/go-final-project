9de5c0f5ba831189635479e125637e186a64d117
Adlan <f0rstudyxx@gmail.com>
2026-04-30 13:26:59 +0300

    step 5 done

--- pkg/api/tasks.go
+++ pkg/api/tasks.go
@@ -12,7 +12,9 @@ type TasksResp struct {
 func tasksHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	tasks, err := db.Tasks(50)
+	search := r.URL.Query().Get("search")
+
+	tasks, err := db.Tasks(50, search)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		writeJson(w, map[string]string{"error": err.Error()})
