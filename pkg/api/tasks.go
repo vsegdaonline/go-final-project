ab23c34f81973a3168509a1431309119ffe11665
Adlan <f0rstudyxx@gmail.com>
2026-04-30 17:51:31 +0300

    step 6 done

--- pkg/api/tasks.go
+++ pkg/api/tasks.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"go1f/pkg/db"
 	"net/http"
 )
@@ -23,4 +26,65 @@ func tasksHandler(w http.ResponseWriter, r *http.Request) {
 	writeJson(w, TasksResp{
 		Tasks: tasks,
 	})
+}
+
+func getTaskHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		err := errors.New("не указан идентификатор")
+		w.WriteHeader(http.StatusBadRequest)
+		writeJson(w, map[string]string{"error": err.Error()})
+		return
+	}
+
+	task, err := db.GetTask(id)
+	if err != nil {
+		err := errors.New("задача не найдена")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeJson(w, map[string]string{"error": err.Error()})
+		return
+	}
+	writeJson(w, task)
+}
+
+func putTaskHandler(w http.ResponseWriter, r *http.Request) {
+	task := db.Task{}
+	var buf bytes.Buffer
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		writeJson(w, map[string]string{"error": err.Error()})
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		writeJson(w, map[string]string{"error": err.Error()})
+		return
+	}
+
+	if task.Title == "" {
+		err = errors.New("не указан заголовок задачи")
+		w.WriteHeader(http.StatusBadRequest)
+		writeJson(w, map[string]string{"error": err.Error()})
+		return
+	}
+
+	if err = checkDate(&task); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		writeJson(w, map[string]string{"error": err.Error()})
+		return
+	}
+
+	err = db.UpdateTask(&task)
+	if err != nil {
+		err = errors.New("задача не найдена")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeJson(w, map[string]string{"error": err.Error()})
+		return
+	}
+	writeJson(w, map[string]string{})
 }
